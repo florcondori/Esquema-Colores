5e1c203a366e55448fe5b54af47bcd3bd584cf90
flor condori <condorihuamanif@gmail.com>
2017-04-19 12:04:33 -0500

    creando la estructura mediante js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
-app
+window.addEventListener("load", function(){
+	document.body.appendChild(crearEstructura());
+});
+
+var arrayColores=[["red","purple","yellow"],["blue","green","black"]];
+function crearEstructura(){
+	var documentFragment = document.createDocumentFragment();
+	arrayColores.forEach(function(elem){
+		var divContenedor = document.createElement("div");
+		divContenedor.classList.add("contenedor");
+		divContenedor.setAttribute("id",elem[0]);
+		var divPadre = document.createElement("div");
+		divPadre.classList.add("padre");
+		divPadre.setAttribute("id",elem[1]);
+		var divHijo = document.createElement("div");
+		divHijo.classList.add("hijo");
+		divHijo.setAttribute("id",elem[2]);
+
+		divContenedor.appendChild(divPadre);
+		divPadre.appendChild(divHijo);
+		documentFragment.appendChild(divContenedor);
+	});
+
+	return documentFragment;
+}
